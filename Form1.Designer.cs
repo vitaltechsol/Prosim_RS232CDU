52db2d4758c6baacc4e53b470a587ba486e54160
Fabian Betancur <info@vts.us>
2025-05-29 22:28:09 -0700

    Update all annunciators to work

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,139 +42,177 @@
             this.lblL4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.lblPort = new System.Windows.Forms.Label();
+            this.lblL5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // connectionStatusLabel
             // 
             this.connectionStatusLabel.AutoSize = true;
-            this.connectionStatusLabel.Location = new System.Drawing.Point(32, 33);
+            this.connectionStatusLabel.Location = new System.Drawing.Point(59, 61);
+            this.connectionStatusLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.connectionStatusLabel.Name = "connectionStatusLabel";
-            this.connectionStatusLabel.Size = new System.Drawing.Size(95, 13);
+            this.connectionStatusLabel.Size = new System.Drawing.Size(177, 25);
             this.connectionStatusLabel.TabIndex = 5;
             this.connectionStatusLabel.Text = "Prosim Connection";
             // 
             // lblConnection
             // 
             this.lblConnection.AutoSize = true;
-            this.lblConnection.Location = new System.Drawing.Point(133, 33);
+            this.lblConnection.Location = new System.Drawing.Point(244, 61);
+            this.lblConnection.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblConnection.Name = "lblConnection";
-            this.lblConnection.Size = new System.Drawing.Size(16, 13);
+            this.lblConnection.Size = new System.Drawing.Size(27, 25);
             this.lblConnection.TabIndex = 6;
             this.lblConnection.Text = "...";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 67);
+            this.label1.Location = new System.Drawing.Point(59, 124);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.Size = new System.Drawing.Size(124, 25);
             this.label1.TabIndex = 7;
             this.label1.Text = "Key Pressed";
             // 
             // lblKeyPressed
             // 
             this.lblKeyPressed.AutoSize = true;
-            this.lblKeyPressed.Location = new System.Drawing.Point(111, 67);
+            this.lblKeyPressed.Location = new System.Drawing.Point(204, 124);
+            this.lblKeyPressed.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblKeyPressed.Name = "lblKeyPressed";
-            this.lblKeyPressed.Size = new System.Drawing.Size(16, 13);
+            this.lblKeyPressed.Size = new System.Drawing.Size(27, 25);
             this.lblKeyPressed.TabIndex = 8;
             this.lblKeyPressed.Text = "...";
             // 
             // lblL1
             // 
             this.lblL1.AutoSize = true;
-            this.lblL1.Location = new System.Drawing.Point(32, 133);
+            this.lblL1.Location = new System.Drawing.Point(173, 228);
+            this.lblL1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblL1.Name = "lblL1";
-            this.lblL1.Size = new System.Drawing.Size(16, 13);
+            this.lblL1.Size = new System.Drawing.Size(27, 25);
             this.lblL1.TabIndex = 9;
             this.lblL1.Text = "...";
             // 
             // L1
             // 
             this.L1.AutoSize = true;
-            this.L1.Location = new System.Drawing.Point(32, 110);
+            this.L1.Location = new System.Drawing.Point(173, 185);
+            this.L1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.L1.Name = "L1";
-            this.L1.Size = new System.Drawing.Size(31, 13);
+            this.L1.Size = new System.Drawing.Size(58, 25);
             this.L1.TabIndex = 10;
             this.L1.Text = "MSG";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(68, 110);
+            this.label3.Location = new System.Drawing.Point(291, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(67, 25);
             this.label3.TabIndex = 11;
             this.label3.Text = "EXEC";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(108, 110);
+            this.label4.Location = new System.Drawing.Point(173, 276);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.Size = new System.Drawing.Size(67, 25);
             this.label4.TabIndex = 12;
-            this.label4.Text = "L3";
+            this.label4.Text = "OFST";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(153, 110);
+            this.label5.Location = new System.Drawing.Point(59, 185);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.Size = new System.Drawing.Size(63, 25);
             this.label5.TabIndex = 13;
-            this.label5.Text = "L4";
+            this.label5.Text = "CALL";
             // 
             // lblL2
             // 
             this.lblL2.AutoSize = true;
-            this.lblL2.Location = new System.Drawing.Point(71, 133);
+            this.lblL2.Location = new System.Drawing.Point(296, 228);
+            this.lblL2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblL2.Name = "lblL2";
-            this.lblL2.Size = new System.Drawing.Size(16, 13);
+            this.lblL2.Size = new System.Drawing.Size(27, 25);
             this.lblL2.TabIndex = 14;
             this.lblL2.Text = "...";
             // 
             // lblL3
             // 
             this.lblL3.AutoSize = true;
-            this.lblL3.Location = new System.Drawing.Point(111, 133);
+            this.lblL3.Location = new System.Drawing.Point(179, 319);
+            this.lblL3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblL3.Name = "lblL3";
-            this.lblL3.Size = new System.Drawing.Size(16, 13);
+            this.lblL3.Size = new System.Drawing.Size(27, 25);
             this.lblL3.TabIndex = 15;
             this.lblL3.Text = "...";
             // 
             // lblL4
             // 
             this.lblL4.AutoSize = true;
-            this.lblL4.Location = new System.Drawing.Point(153, 133);
+            this.lblL4.Location = new System.Drawing.Point(59, 228);
+            this.lblL4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblL4.Name = "lblL4";
-            this.lblL4.Size = new System.Drawing.Size(16, 13);
+            this.lblL4.Size = new System.Drawing.Size(27, 25);
             this.lblL4.TabIndex = 16;
             this.lblL4.Text = "...";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(284, 33);
+            this.label2.Location = new System.Drawing.Point(521, 61);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.Size = new System.Drawing.Size(67, 25);
             this.label2.TabIndex = 17;
             this.label2.Text = "PORT";
             // 
             // lblPort
             // 
             this.lblPort.AutoSize = true;
-            this.lblPort.Location = new System.Drawing.Point(327, 33);
+            this.lblPort.Location = new System.Drawing.Point(600, 61);
+            this.lblPort.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblPort.Name = "lblPort";
-            this.lblPort.Size = new System.Drawing.Size(16, 13);
+            this.lblPort.Size = new System.Drawing.Size(27, 25);
             this.lblPort.TabIndex = 18;
             this.lblPort.Text = "...";
             // 
+            // lblL5
+            // 
+            this.lblL5.AutoSize = true;
+            this.lblL5.Location = new System.Drawing.Point(59, 319);
+            this.lblL5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblL5.Name = "lblL5";
+            this.lblL5.Size = new System.Drawing.Size(27, 25);
+            this.lblL5.TabIndex = 20;
+            this.lblL5.Text = "...";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 276);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 25);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "FAIL";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 256);
+            this.ClientSize = new System.Drawing.Size(814, 473);
+            this.Controls.Add(this.lblL5);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.lblPort);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblL4);
@@ -189,6 +227,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblConnection);
             this.Controls.Add(this.connectionStatusLabel);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -213,6 +252,8 @@
         private System.Windows.Forms.Label lblL4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblL5;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,16 @@ namespace Prosim_RS232CDU
         string comPort = "COM3";
         int baudRate = 9600;
 
+        // Control flags 
+        const ushort CTRL_MSG = 0x0001;
+        const ushort CTRL_EXEC = 0x0002;
+        const ushort CTRL_OFST = 0x0004;
+        const ushort CTRL_CALL = 0x0010;
+        const ushort CTRL_FAIL = 0x0020;
+
+        static ushort message = 0xD300;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -30,11 +40,14 @@ namespace Prosim_RS232CDU
             DataRef dataRef1 = new DataRef("system.indicators.I_CDU1_MSG", 100, prosimConnect);
             DataRef dataRef2 = new DataRef("system.indicators.I_CDU1_EXEC", 100, prosimConnect);
             DataRef dataRef3 = new DataRef("system.indicators.I_CDU1_FAIL", 100, prosimConnect);
+            DataRef dataRef4 = new DataRef("system.indicators.I_CDU1_OFFSET", 100, prosimConnect);
+            DataRef dataRef5 = new DataRef("system.indicators.I_CDU1_CALL", 100, prosimConnect);
 
             dataRef1.onDataChange += AnnunciatorHandler;
             dataRef2.onDataChange += AnnunciatorHandler;
             dataRef3.onDataChange += AnnunciatorHandler;
-
+            dataRef4.onDataChange += AnnunciatorHandler;
+            dataRef5.onDataChange += AnnunciatorHandler;
         }
 
         void connection_onConnect()
@@ -53,13 +66,11 @@ namespace Prosim_RS232CDU
             serialPort.DataReceived += SerialPort_DataReceived;
             serialPort.Open();
             lblPort.Text = "Listening on " + comPort;
-            Console.WriteLine("Listening on COM8...");
+            Console.WriteLine("Listening on" + comPort);
         }
 
-        //
         void ConnectToProSim(string prosimIP)
         {
-
             try
             {
                 prosimConnect.Connect(prosimIP);
@@ -72,8 +83,6 @@ namespace Prosim_RS232CDU
         void StartConnection()
         {
             lblConnection.Text = "Connected";
-
-    
         }
 
         void EndConnection()
@@ -107,20 +116,42 @@ namespace Prosim_RS232CDU
                         if (hex.Equals("F070", StringComparison.OrdinalIgnoreCase))
                         {
                             Console.WriteLine("CDU MENU button press detected. Sending to ProSim...");
-                            //lblKeyPressed.Invoke(new MethodInvoker(delegate
-                            //{
-                            //    lblKeyPressed.Text = "MENU";
-                            //}));
 
-                            DataRef dataRef = new DataRef("system.switches.S_CDU2_KEY_MENU", 100, prosimConnect, true);
-                            // ON
-                            dataRef.value = 1;
+                            DataRef dataRef = new DataRef("system.switches.S_CDU1_KEY_MENU", 100, prosimConnect, true);
 
-                            // Trigger CDU MENU button press
-                            System.Threading.Thread.Sleep(100);
+                            dataRef.value = 1; // ON
 
-                            // OFF
-                            dataRef.value = 0;
+                            System.Threading.Thread.Sleep(100); // Trigger utton press
+
+                            dataRef.value = 0; // OFF
+                            Console.WriteLine("CDU MENU button press sent to ProSim.");
+                        }
+
+                        if (hex.Equals("F029", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("CDU MENU button press detected. Sending to ProSim...");
+
+                            DataRef dataRef = new DataRef("system.switches.S_CDU1_KEY_LSK1L", 100, prosimConnect, true);
+
+                            dataRef.value = 1; // ON
+
+                            System.Threading.Thread.Sleep(100); // Trigger button press
+
+                            dataRef.value = 0; // OFF
+                            Console.WriteLine("CDU MENU button press sent to ProSim.");
+                        }
+
+                        if (hex.Equals("F028", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("CDU MENU button press detected. Sending to ProSim...");
+
+                            DataRef dataRef = new DataRef("system.switches.S_CDU1_KEY_LSK2L", 100, prosimConnect, true);
+
+                            dataRef.value = 1; // ON
+
+                            System.Threading.Thread.Sleep(100); // Trigger button press
+
+                            dataRef.value = 0; // OFF
                             Console.WriteLine("CDU MENU button press sent to ProSim.");
                         }
                     }
@@ -132,87 +163,223 @@ namespace Prosim_RS232CDU
             }
         }
 
-        // Handler for ProSim dataref change
 
+        // Annunciator handler using bitwise TurnOn/TurnOff
         private void AnnunciatorHandler(DataRef dataRef)
         {
             var value = Convert.ToInt32(dataRef.value);
-            // 2 is on
-            // 0 is off
+            Console.WriteLine($"DataRef {dataRef.name} changed to: {value}");
 
-            Console.WriteLine($"ProSim MSG Annunciator DataRef {dataRef.name} changed to: {value}");
+            ushort flag = 0;
+            Label targetLabel = lblL1;
 
-            switch(dataRef.name)
+            switch (dataRef.name)
             {
-
                 case "system.indicators.I_CDU1_MSG":
-                    if (value == 2)
-                    {
-                        Console.WriteLine("MSG annunciator ON - sending D3 01 to COM PORT...");
-                        SendHexMessage(new byte[] { 0xD3, 0x01 }); // <-- Add correct value
-                        lblL1.Invoke(new MethodInvoker(delegate
-                        {
-                            lblL1.Text = "ON";
-                        }));
-                    }
-
-                    if (value == 0)
-                    {
-                        Console.WriteLine("MSG annunciator OFF - sending D3 01 to COM PORT...");
-                        SendHexMessage(new byte[] { 0xD3, 0x01 }); // <-- Add correct value
-                        lblL1.Invoke(new MethodInvoker(delegate
-                        {
-                            lblL1.Text = "OFF";
-                        }));
-                    }
+                    flag = CTRL_MSG;
+                    targetLabel = lblL1;
                     break;
                 case "system.indicators.I_CDU1_EXEC":
-                    if (value == 2)
-                    {
-                        Console.WriteLine("EXEC annunciator ON - sending D3 01 to COM PORT...");
-                        SendHexMessage(new byte[] { 0xD3, 0x01 }); // <-- Add correct value
-                        lblL2.Invoke(new MethodInvoker(delegate
-                        {
-                            lblL2.Text = "ON";
-                        }));
-                    }
-
-                    if (value == 0)
-                    {
-                        Console.WriteLine("EXEC annunciator OFF - sending D3 01 to COM PORT...");
-                        SendHexMessage(new byte[] { 0xD3, 0x01 }); // <-- Add correct value
-                        lblL2.Invoke(new MethodInvoker(delegate
-                        {
-                            lblL2.Text = "OFF";
-                        }));
-                    }
+                    flag = CTRL_EXEC;
+                    targetLabel = lblL2;
+                    break;
+                case "system.indicators.I_CDU1_OFFSET":
+                    flag = CTRL_OFST;
+                    targetLabel = lblL3;
+                    break;
+                case "system.indicators.I_CDU1_CALL":
+                    flag = CTRL_CALL;
+                    targetLabel = lblL4;
+                    break;
+                case "system.indicators.I_CDU1_FAIL":
+                    flag = CTRL_FAIL;
+                    targetLabel = lblL5;
                     break;
             }
 
+            if (flag != 0)
+            {
+                if (value == 2) // ON
+                {
+                    TurnOn(flag);
+                    targetLabel.Invoke(new MethodInvoker(() => targetLabel.Text = "ON"));
+                }
+                else if (value == 0) // OFF
+                {
+                    TurnOff(flag);
+                    targetLabel.Invoke(new MethodInvoker(() => targetLabel.Text = "OFF"));
+                }
+            }
         }
 
 
-        // Write D3 01 to COM8
-        private static void SendHexMessage(byte[] data)
+        /*		// Handler for ProSim dataref change
+
+				private void AnnunciatorHandler(DataRef dataRef)
+				{
+					var value = Convert.ToInt32(dataRef.value);
+					// 2 is on
+					// 0 is off
+
+					Console.WriteLine($"ProSim MSG Annunciator DataRef {dataRef.name} changed to: {value}");
+
+					switch (dataRef.name)
+					{
+
+						case "system.indicators.I_CDU1_MSG":
+							if (value == 2)
+							{
+								Console.WriteLine("MSG annunciator ON - sending D3 01 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x01 }); // <-- Add correct value
+								lblL1.Invoke(new MethodInvoker(delegate
+								{
+									lblL1.Text = "ON";
+								}));
+							}
+
+							if (value == 0)
+							{
+								Console.WriteLine("MSG annunciator OFF - sending D3 01 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x00 }); // <-- Add correct value
+								lblL1.Invoke(new MethodInvoker(delegate
+								{
+									lblL1.Text = "OFF";
+								}));
+							}
+							break;
+
+						case "system.indicators.I_CDU1_EXEC":
+							if (value == 2)
+							{
+								Console.WriteLine("EXEC annunciator ON - sending D3 02 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x02 }); // <-- Add correct value
+								lblL2.Invoke(new MethodInvoker(delegate
+								{
+									lblL2.Text = "ON";
+								}));
+							}
+
+							if (value == 0)
+							{
+								Console.WriteLine("EXEC annunciator OFF - sending D3 02 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x00 }); // <-- Add correct value
+								lblL2.Invoke(new MethodInvoker(delegate
+								{
+									lblL2.Text = "OFF";
+								}));
+							}
+							break;
+
+						case "system.indicators.I_CDU1_OFFSET":
+							if (value == 2)
+							{
+								Console.WriteLine("OFFSET annunciator ON - sending D3 04 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x04 }); // <-- Add correct value
+								lblL2.Invoke(new MethodInvoker(delegate
+								{
+									lblL3.Text = "ON";
+								}));
+							}
+
+							if (value == 0)
+							{
+								Console.WriteLine("OFFSET annunciator OFF - sending D3 04 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x00 }); // <-- Add correct value
+								lblL2.Invoke(new MethodInvoker(delegate
+								{
+									lblL3.Text = "OFF";
+								}));
+							}
+							break;
+
+						case "system.indicators.I_CDU1_CALL":
+							if (value == 2)
+							{
+								Console.WriteLine("CALL annunciator ON - sending D3 10 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x10 }); // <-- Add correct value
+								lblL2.Invoke(new MethodInvoker(delegate
+								{
+									lblL4.Text = "ON";
+								}));
+							}
+
+							if (value == 0)
+							{
+								Console.WriteLine("CALL annunciator OFF - sending D3 10 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x00 }); // <-- Add correct value
+								lblL2.Invoke(new MethodInvoker(delegate
+								{
+									lblL4.Text = "OFF";
+								}));
+							}
+							break;
+
+						case "system.indicators.I_CDU1_FAIL":
+							if (value == 2)
+							{
+								Console.WriteLine("FAIL annunciator ON - sending D3 20 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x20 }); // <-- Add correct value
+								lblL2.Invoke(new MethodInvoker(delegate
+								{
+									lblL4.Text = "ON";
+								}));
+							}
+
+							if (value == 0)
+							{
+								Console.WriteLine("FAIL annunciator OFF - sending D3 20 to COM PORT...");
+								SendHexMessage(new byte[] { 0xD3, 0x00 }); // <-- Add correct value
+								lblL2.Invoke(new MethodInvoker(delegate
+								{
+									lblL4.Text = "OFF";
+								}));
+							}
+							break;
+					}
+
+				}
+		*/
+
+        // Write to COM8
+        private static void Send()
         {
+            byte[] bytes = new byte[]
+            {
+                (byte)((message >> 8) & 0xFF),
+                (byte)(message & 0xFF)
+            };
+
             try
             {
                 if (serialPort != null && serialPort.IsOpen)
                 {
-                    serialPort.Write(data, 0, data.Length);
-                    Console.WriteLine("D3 01 sent to COM PORT.");
+                    serialPort.Write(bytes, 0, 2);
+                    Console.WriteLine($"Sent: 0x{message:X4} as {bytes[0]:X2}-{bytes[1]:X2}");
                 }
                 else
                 {
-                    Console.WriteLine("Serial port is not open.");
+                    Console.WriteLine("Serial port not open.");
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error writing to PORT: {ex.Message}");
+                Console.WriteLine($"Error writing to port: {ex.Message}");
             }
         }
 
-       
+        // Turn ON flag (set bit)
+        private static void TurnOn(ushort flag)
+        {
+            message |= flag;
+            Send();
+        }
+
+        // Turn OFF flag (clear bit)
+        private static void TurnOff(ushort flag)
+        {
+            message &= (ushort)~flag;
+            Send();
+        }
+
     }
 }
